552309be7ab8db96618cf73478105808250c98b0
Rohit Jha <rohit305jha@gmail.com>
2018-08-10 22:14:48 -0700

    added BATCH_STOCK_QUOTES

--- alphavantage/Stock.py
+++ alphavantage/Stock.py
@@ -7,12 +7,11 @@ class Stock():
         self.api_key = api_key
         self.base_url = 'https://www.alphavantage.co/query'
 
-    async def _get_data_response(self, function: str, symbol: str, interval: str, output_size: str, data_type: str) -> str:
-        self._validate_params(function, symbol, data_type)
+    async def _get_data_response(self, function: str, symbol: str, symbols: list, interval: str, output_size: str, data_type: str) -> str:
+        self._validate_params(function, data_type)
 
         params = {
             'function': function,
-            'symbol': symbol,
             'datatype': data_type,
             'apikey': self.api_key
         }
@@ -21,40 +20,41 @@ class Stock():
             params['interval'] = interval
         if output_size is not None and len(output_size) > 0:
             params['outputsize'] = output_size
+        if symbol is not None and len(symbol) > 0:
+            params['symbol'] = symbol
+        if symbols is not None and len(symbols) > 0:
+            params['symbols'] = ','.join(symbols)
 
         async with aiohttp.ClientSession() as session:
             async with session.get(self.base_url, params=params) as response:
                 return await response.text()
 
-    def _validate_params(self, function, symbol, data_type):
+    def _validate_params(self, function, data_type):
         if function is None or len(function) == 0:
             raise Exception('Invalid parameter `function`.')
-        if symbol is None or len(symbol) == 0:
-            raise Exception('Invalid parameter `symbol`.')
-        if data_type not in ['json', 'csv'] and data_type is not None:
-            raise Exception(
-                'Invalid parameter `data_type`. Acceptable values are `json` and `csv`.')
+        if data_type not in['json', 'csv'] and data_type is not None:
+            raise Exception('Invalid parameter `data_type`. Acceptable values are `json` and `csv`.')
 
     async def intraday(self, symbol: str, interval_minutes=60, output_size='compact', data_type='json') -> str:
-        return await self._get_data_response('TIME_SERIES_INTRADAY', symbol, str(interval_minutes) + 'min', output_size, data_type)
+        return await self._get_data_response('TIME_SERIES_INTRADAY', symbol, None, str(interval_minutes) + 'min', output_size, data_type)
 
     async def daily(self, symbol: str, output_size='compact', data_type='json') -> str:
-        return await self._get_data_response('TIME_SERIES_DAILY', symbol, None, output_size, data_type)
+        return await self._get_data_response('TIME_SERIES_DAILY', symbol, None, None, output_size, data_type)
 
     async def daily_adjusted(self, symbol: str, output_size='compact', data_type='json') -> str:
-        return await self._get_data_response('TIME_SERIES_DAILY_ADJUSTED', symbol, None, output_size, data_type)
+        return await self._get_data_response('TIME_SERIES_DAILY_ADJUSTED', symbol, None, None, output_size, data_type)
 
     async def weekly(self, symbol: str, output_size='compact', data_type='json') -> str:
-        return await self._get_data_response('TIME_SERIES_WEEKLY', symbol, None, output_size, data_type)
+        return await self._get_data_response('TIME_SERIES_WEEKLY', symbol, None, None, output_size, data_type)
 
     async def weekly_adjusted(self, symbol: str, output_size='compact', data_type='json') -> str:
-        return await self._get_data_response('TIME_SERIES_WEEKLY_ADJUSTED', symbol, None, output_size, data_type)
+        return await self._get_data_response('TIME_SERIES_WEEKLY_ADJUSTED', symbol, None, None, output_size, data_type)
 
     async def monthly(self, symbol: str, output_size='compact', data_type='json') -> str:
-        return await self._get_data_response('TIME_SERIES_MONTHLY', symbol, None, output_size, data_type)
+        return await self._get_data_response('TIME_SERIES_MONTHLY', symbol, None, None, output_size, data_type)
 
     async def monthly_adjusted(self, symbol: str, output_size='compact', data_type='json') -> str:
-        return await self._get_data_response('TIME_SERIES_MONTHLY_ADJUSTED', symbol, None, output_size, data_type)
+        return await self._get_data_response('TIME_SERIES_MONTHLY_ADJUSTED', symbol, None, None, output_size, data_type)
 
-    async def batch_quotes(self, symbol: str, data_type='json') -> str:
-        return await self._get_data_response('TIME_SERIES_MONTHLY_ADJUSTED', symbol, None, None, data_type)
+    async def batch_quotes(self, symbols: list, data_type='json') -> str:
+        return await self._get_data_response('BATCH_STOCK_QUOTES', None, symbols, None, None, data_type)
--- example.py
+++ example.py
@@ -5,7 +5,7 @@ import alphavantage
 client = alphavantage.Stock('AC08TL1JSK9Q6YUH')
 
 loop = asyncio.get_event_loop()
-data = loop.run_until_complete(client.daily_adjusted('GDDY'))
+data = loop.run_until_complete(client.batch_quotes(['GDDY', 'MSFT']))
 loop.close()
 
 print(data)
